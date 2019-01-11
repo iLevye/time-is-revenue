2aca12e792f2e11c79cfefd05e9a1737fbfbd114
Nuri Can Aktürk <nuricanakturk@gmail.com>
2019-01-11 23:24:27 +0300

    added new pages

--- public/html/login.php
+++ public/html/login.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width"/>
-		<link rel="stylesheet" type="text/css" href="assets/css/reset.css?v=<?=time()?>"/>
+		<?php include 'inc/meta.php'; ?>
 		<link rel="stylesheet" type="text/css" href="assets/css/login.css?v=<?=time()?>"/>
 		<title>Woney | Login</title>
 	</head>
