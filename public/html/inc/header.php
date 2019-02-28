b450b73a7e8030a8af0b90a0c24c1a3afffa2c9f
Nuri Can Aktürk <nuricanakturk@gmail.com>
2019-02-28 14:58:25 +0300

    Added new ui pages

---
+++ public/html/inc/header.php
@@ -0,0 +1,15 @@
+<header>
+	
+	<div class="wrapper">
+		
+		<a id="logo">Woney</a>
+
+		<nav>
+			<a class="nav-link" href="/task/">Task List</a>
+		</nav>
+
+	</div>
+
+</header>
+
+<div id="header-clear"></div>
